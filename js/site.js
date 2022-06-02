ad4e442d7455526f85c6fbe249e5801ceb2bbe68
JonathanKayDev <67706563+JonathanKay86@users.noreply.github.com>
2022-06-02 09:59:28 +0100

    Refactor code and update views

--- js/site.js
+++ js/site.js
@@ -4,19 +4,17 @@ function getString() {
     let userString = document.getElementById("userString").value;
     document.getElementById("alert").classList.add("invisible");
 
+    // check for palindrome, if valid input
     if (validateString(userString)) {
         let inputString = simplifyString(userString);
         let outputString = reverseString(inputString);
 
-        if (checkPalindrome(inputString, outputString)) {
-            displayString(`YES! '${inputString}' is a PALINDROME!`);
-        }
-        else
-        {
-            displayString(`NO! '${inputString}' is NOT a PALINDROME!`);
-        }
-    }
+        // check for palindrom
+        let returnObj = checkPalindrome(inputString, outputString);
 
+        // display output message 
+        displayString(returnObj);
+    }
 }
 
 
@@ -24,6 +22,9 @@ function getString() {
 // LOGIC FUNCTION(S)
 function validateString(inputString) {
     let output = true;
+
+    // strip out space a non-letters
+    inputString = simplifyString(inputString); 
 
     // check if string is empty or has only white spaces
     if (inputString.length === 0 || !inputString.trim()) {
@@ -40,7 +41,7 @@ function simplifyString(inputString){
     let simpleString = "";
 
     simpleString = inputString.toLowerCase();
-    simpleString = simpleString.replace(/[^a-zA-Z]/g,"");
+    simpleString = simpleString.replace(/[^a-z0-9]/g,"");
     simpleString = simpleString.replaceAll(/\s+/g,"");
 
     return simpleString;
@@ -60,21 +61,28 @@ function reverseString(inputString) {
 
 // check for palindrome
 function checkPalindrome(inputString, reverseString) {
+    let returnObj = {};
+
     if (inputString == reverseString) {
-        return true;
+        returnObj.msg = `YES! Palindrome found!`;
     }
     else
     {
-        return false;
+        returnObj.msg = `Sorry! No palindrome found.`;
     }
+
+    returnObj.reversed = reverseString;
+
+    return returnObj;
 }
 
 
 // VIEW FUNCTION(S)
 // Display the output string to the user
-function displayString(inputString) {
+function displayString(returnObj) {
     // write message to page
-    document.getElementById("msg").innerHTML = inputString;
+    document.getElementById("alertHeader").innerHTML = returnObj.msg;
+    document.getElementById("msg").innerHTML = `Your phrase reversed is: ${returnObj.reversed}`;
 
     // show the alert box
     document.getElementById("alert").classList.remove("invisible");
